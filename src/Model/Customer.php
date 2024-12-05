7aaa821bb9c1fcbe223483ff2182c5100272139b
ihsanfikri12 <fikriabiyyu@gmail.com>
2024-12-05 17:12:20 +0700

    fixing bug no hp

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -14,12 +14,18 @@ class Customer
     ) {
         Assert::nullOrEmail($this->email);
 
-        if (
-            !is_null($phone) &&
-            !(str_starts_with($phone, '62') || str_starts_with($phone, '+62') || str_starts_with($phone, '08')) ||
-            strlen($phone) > 13
-        ) {
-            throw new \InvalidArgumentException('Nomor telepon harus diawali dengan 62, +62, atau 08, dan maksimal 13 karakter.');
+        if (!is_null($this->phone)) {
+
+            if (str_starts_with($this->phone, '0')) {
+                $this->phone = '+62' . substr($this->phone, 1);
+            }
+
+            if (
+                !(str_starts_with($this->phone, '62') || str_starts_with($this->phone, '+62')) ||
+                strlen($this->phone) > 15
+            ) {
+                throw new \InvalidArgumentException('Nomor telepon harus diawali dengan 62, +62, atau 0, dan maksimal 13 karakter.');
+            }
         }
     }
 }
