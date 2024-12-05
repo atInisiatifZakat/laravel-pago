4d4d0e2b67c70308d67ec5cdf7d3a1553e7083bb
ihsanfikri12 <fikriabiyyu@gmail.com>
2024-12-05 16:56:57 +0700

    fixing assert

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -11,10 +11,15 @@ class Customer
         public ?string $email = null,
         public ?string $phone = null,
         public ?string $address = null,
-    )
-    {
+    ) {
         Assert::nullOrEmail($this->email);
-        Assert::nullOrStartsWith($this->phone, '62');
-        Assert::nullOrLength($this->phone, 13);
+
+        if (
+            !is_null($phone) &&
+            !(str_starts_with($phone, '62') || str_starts_with($phone, '+62') || str_starts_with($phone, '08')) ||
+            strlen($phone) > 13
+        ) {
+            throw new \InvalidArgumentException('Nomor telepon harus diawali dengan 62, +62, atau 08, dan maksimal 13 karakter.');
+        }
     }
 }
