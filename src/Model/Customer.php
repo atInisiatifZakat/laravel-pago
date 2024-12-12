d7dd124690983dc1e523eb99ca5da7ba9d429f31
Nuradiyana <me@nooradiana.com>
2024-12-12 22:24:19 +0700

    Change length validation to 15 karakter (#18)
    * Change length validation
    
    * Update Customer.php

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -15,16 +15,17 @@ class Customer
         Assert::nullOrEmail($this->email);
 
         if (!is_null($this->phone)) {
+            $this->phone = \trim($this->phone);
 
             if (str_starts_with($this->phone, '0')) {
-                $this->phone = '+62' . substr($this->phone, 1);
+                $this->phone = \trim('+62' . substr($this->phone, 1));
             }
 
             if (
                 !(str_starts_with($this->phone, '62') || str_starts_with($this->phone, '+62')) ||
-                strlen($this->phone) > 13
+                strlen($this->phone) > 15
             ) {
-                throw new \InvalidArgumentException('Nomor telepon harus diawali dengan 62, +62, atau 0, dan maksimal 13 karakter.');
+                throw new \InvalidArgumentException('Nomor telepon harus diawali dengan 62, +62, atau 0, dan maksimal 15 karakter.');
             }
         }
     }
